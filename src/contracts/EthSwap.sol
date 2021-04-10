e15259b55f38f879920bddd649d456cab9411bb7
livdevan <livdevan@gmail.com>
2021-04-10 14:58:26 +0200

    adds buyTokens function to EthSwap contract

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -6,9 +6,32 @@ import "./Token.sol";
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
     Token public token;
+    uint public rate = 100;
+
+    event TokensPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+    
 
     constructor(Token _token) public {
-    token = _token;
+        token = _token;
+    }
+    
+    function buyTokens() public payable {
+        // Calculate the number of tokens to buy
+        uint tokenAmount = msg.value * rate;
+
+        // Require that EthSwap has enough tokens
+        require(token.balanceOf(address(this)) >= tokenAmount);
+
+        // Transfer tokens to the user
+        token.transfer(msg.sender, tokenAmount);
+
+        // Emit an event
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
     }
     
 }
